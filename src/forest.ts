9d80018887a132ec52c447f0be119df9099d3c69
haruki-nikaidou <meteo.haru@gmail.com>
2024-01-03 14:23:55 +0000

    impl OnedriveForestFs class

--- src/forest.ts
+++ src/forest.ts
@@ -1,33 +1,84 @@
-import {OnedriveAccount} from "./types/odTree";
-import OdTree from "./index";
-import {back, cd, Directory, fromOdTree, mkdir} from "./fileSystem";
+import {addFile, back, cd, combineDirectory, cp, Directory, File, fromOdTree, mkdir, rename, rm} from "./fileSystem";
+import {DirectoryNode} from "./types/odTree";
 
 export class OnedriveTreeFs {
-    root?: Directory;
-    ready: Promise<void> = new Promise(() => {});
+    root: Directory;
 
-    constructor(onedriveAccount: OnedriveAccount) {
-        const odTree = OdTree(onedriveAccount.clientId, onedriveAccount.clientSecret, onedriveAccount.tenantId);
-        odTree.then(tree => {
-            this.root = fromOdTree(tree);
-            this.ready = Promise.resolve();
-        });
+    constructor(tree: DirectoryNode) {
+        this.root = fromOdTree(tree);
     }
 
-    async cd(path: symbol[]) {
-        await this.ready;
-        const root = this.root!;
-        return cd(path, root);
+    cd(path: symbol[]) {
+        return cd(path, this.root);
     }
-    async back(path: symbol[]) {
-        await this.ready;
-        const root = this.root!;
-        return back(path, root);
+
+    back(path: symbol[]) {
+        return back(path, this.root);
     }
-    async mkdir(path: symbol[], name: string) {
-        await this.ready;
-        const root = this.root!;
-        return mkdir(path, root, name);
+
+    mkdir(path: symbol[], name: string) {
+        return mkdir(path, this.root, name);
+    }
+
+    rm(path: symbol[]) {
+        return rm(path, this.root);
+    }
+
+    cp(path: symbol[], newPath: symbol[]) {
+        return cp(path, this.root, newPath);
+    }
+
+    mv(path: symbol[], newPath: symbol[]) {
+        this.cp(path, newPath);
+        this.rm(path);
+    }
+
+    addFile(path: symbol[], file: File) {
+        addFile(path, this.root, file);
+    }
+
+    rename(path: symbol[], newName: string) {
+        rename(path, this.root, newName);
+    }
+
+    static combine(fs1: OnedriveTreeFs, fs2: OnedriveTreeFs): OnedriveTreeFs {
+        const root = new OnedriveTreeFs({
+            name: 'root',
+            type: 'directory',
+            children: [],
+        } as DirectoryNode);
+        root.root = combineDirectory(fs1.root, fs2.root);
+        return root;
     }
 }
 
+export default class OnedriveForestFs extends OnedriveTreeFs {
+    constructor(trees: DirectoryNode[]) {
+        super({
+            name: 'root',
+            type: 'directory',
+            children: [],
+        } as DirectoryNode);
+        const roots = trees.map(tree => fromOdTree(tree));
+        this.root = mergeArray(roots, combineDirectory);
+    }
+}
+
+function mergePairs<T>(array: T[], mergeFunction: (a: T, b: T) => T): T[] {
+    let merged = [];
+    for (let i = 0; i < array.length; i += 2) {
+        if (i + 1 < array.length) {
+            merged.push(mergeFunction(array[i], array[i + 1]));
+        } else {
+            merged.push(array[i]);
+        }
+    }
+    return merged;
+}
+
+function mergeArray<T>(array: T[], mergeFunction: (a: T, b: T) => T): T {
+    while (array.length > 1) {
+        array = mergePairs(array, mergeFunction);
+    }
+    return array[0];
+}
