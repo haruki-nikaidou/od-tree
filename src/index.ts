c708dce61d015bb424d7ede81d43e7fb4ea8282e
haruki <meteo.haru@gmail.com>
2024-01-06 14:09:37 +0000

    add export for all function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import {getAccessToken, getOneDriveDriveId} from "./auth";
+import OnedriveForestFs, { OnedriveTreeFs } from "./forest";
 import {buildTree} from "./recursive";
 import {recursiveBuildDirectory} from "./recursive";
-import {FileTree} from "./types/odTree";
+import type {FileNodeType, FileNode, FileTree,DirectoryNode, DriveToken,OnedriveAccount} from "./types/odTree";
+import * as fileSystem from './fileSystem'
 
 export default async function OdTree(clientId: string, clientSecret: string, tenantId: string): Promise<FileTree> {
     const accessToken = await getAccessToken(clientId, clientSecret, tenantId);
@@ -12,9 +14,22 @@ export default async function OdTree(clientId: string, clientSecret: string, ten
     })
 }
 
+const auth = {
+    getAccessToken, getOneDriveDriveId
+}
+
+const recursiveBuild = {
+    buildTree, recursiveBuildDirectory
+}
+
+const forest = {
+    OnedriveForestFs,
+    OnedriveTreeFs,
+    fileSystem
+}
+
 export {
-    getAccessToken,
-    getOneDriveDriveId,
-    buildTree,
-    recursiveBuildDirectory,
+    auth, recursiveBuild, forest,
+    FileNodeType, FileNode, DirectoryNode, DriveToken, FileTree,
+    OnedriveAccount
 }
